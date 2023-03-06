8ab32b1bc0d4b1d7ce859c207e3f278b2d8fe890
yorck25 <yorck.dombrowsky@gmail.com>
2023-03-06 21:49:45 +0100

    read dat out of json

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Plan from './components/Plan';
+import data from './data/response.json';
 
 function App() {
   return (
     <div>
       <Plan />
+      <br></br>
+
+      {
+        data.Rows && data.Rows.map( data => {
+          return(
+            <div className='box'>
+              {data.Name}
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
--- react-app/src/components/Plan.js
+++ react-app/src/components/Plan.js
@@ -4,6 +4,8 @@ import './style.css';
 function Plan() {
   return (
     <div>Plan</div>
+
+
   )
 }
 
