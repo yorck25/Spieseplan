c13ff7cc09100ec262e3a1588cb2c034bae5eff5
yorck25 <yorck.dombrowsky@gmail.com>
2023-03-06 21:31:38 +0100

    set up

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Plan from './components/Plan';
+
+function App() {
+  return (
+    <div>
+      <Plan />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/Plan.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css';
+
+function Plan() {
+  return (
+    <div>Plan</div>
+  )
+}
+
+export default Plan
