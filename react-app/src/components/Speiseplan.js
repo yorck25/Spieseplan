cab7f28c95837e9f9477598f8b5461ff9bcbc7c7
yorck25 <yorck.dombrowsky@gmail.com>
2023-03-08 20:57:40 +0100

    dsgin der Tabelle

--- react-app/src/components/Speiseplan.js
+++ react-app/src/components/Speiseplan.js
@@ -2,37 +2,36 @@ import React, { useState, useEffect } from "react";
 import data from '../data/response.json';
 import './style.css'
 
+
 function Speiseplan() {
     const [tableData, setTableData] = useState([]);
-    var price;
     useEffect(() => {
       // convert data from json file to table format
       const tableRows = data.Rows.map((row) => {
         const rowData = {};;
         rowData["Name"] = row.Name;
         row.Days.forEach((day) => {
-  
           const productId = day.ProductIds[0].ProductId;
           const product = data.Products[productId];
           rowData[`Weekday${day.Weekday}`] = productId;
-          rowData[`Allergens${day.Weekday}`] = product.AllergenIds;
+          rowData[`Allergens${day.Weekday}`] = product.AllergenIds.join(', ');
           rowData[`ProductName${day.Weekday}`] = product.Name;
           rowData[`Price${day.Weekday}`] = product.Price.Betrag;
-  
-          // rowData[`Weekday${day.Weekday}`] = day.ProductIds[0].ProductId;
-          // allergien = data.Products[day.ProductIds[0].ProductId].AllergenIds;
         });
         return rowData;
       });
-  
       setTableData(tableRows);
     }, []);
+
+    
     return (
       <div>
         <section className="planSection">
         <div>
         </div>
+        <h1 className="headline">Essenplan für die nächste Woche:</h1>
         <table>
+          <br></br>
           <thead>
             <tr>
               <th>Name</th>
@@ -43,15 +42,15 @@ function Speiseplan() {
               <th>Friday</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody> 
             {tableData.map((row, index) => (
                 <tr key={index}>
                 <td>{row.Name}</td>
-                <td>{row.ProductName0}<br/>Price: {row.Price0}</td>
-                <td>{row.ProductName1}<br/>Price: {row.Price1}</td>
-                <td>{row.ProductName2}<br/>Price: {row.Price2}</td>
-                <td>{row.ProductName3}<br/>Price: {row.Price3}</td>
-                <td>{row.ProductName4}<br/>Price: {row.Price4}</td>
+                <td>{row.ProductName0}<br/>Price: {row.Price0} € <br/> Allergens: {row.Allergens0}</td>
+                <td>{row.ProductName1}<br/>Price: {row.Price1} €<br/> Allergens: {row.Allergens1}</td>
+                <td>{row.ProductName2}<br/>Price: {row.Price2} € <br/> Allergens: {row.Allergens2}</td>
+                <td>{row.ProductName3}<br/>Price: {row.Price3} € <br/> Allergens: {row.Allergens3}</td>
+                <td>{row.ProductName4}<br/>Price: {row.Price4} € <br className="distance"/> Allergens: {row.Allergens4}</td>
               </tr>
             ))}
           </tbody>
